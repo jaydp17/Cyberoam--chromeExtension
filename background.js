b149899a2e01a359321b200bc3e4be7826281370
jaydp17 <j.solanki17@yahoo.com>
2013-01-02 06:01:43 +0530

    works fine

--- background.js
+++ background.js
@@ -1,34 +1,72 @@
 // this js file will be loaded when the browser starts
 
 var loggedin = false;
+//var theUrl = "http://10.100.56.55:8090/httpclient.html";
+var theUrl = "http://localhost/cyb.php";
 
-console.log('started');
-//loginHandler();
 
+checkConnection(login);
 chrome.browserAction.onClicked.addListener(function(tab){
     if(loggedin){
-	checkConnection(logoutHandler);
+	checkConnection(logout);
     } else {
-	checkConnection(loginHandler);
+	checkConnection(login);
     }
 } );
 
-function loginHandler(){
-    var op = login();
-    if(op == false){
-	alert("Error: login credentials not defined");
-    } else {
-	loggedin = true;
-	chrome.browserAction.setIcon({path:"icon.png"});
-    }
+function login(){
+    var storage = chrome.storage.local;
+    storage.get(null,function(data){
+	if(!data.user || !data.pass){
+	    console.log("Login : crendentials not defined");
+	    alert("Error: login credentials not defined");
+	    return;
+	}
+	$.post(theUrl, { mode:"191", username:data.user,password:data.pass }, function(response){
+	    console.log("Login response :" + response);
+	    loggedin = true;
+	    chrome.browserAction.setIcon({path:"icon.png"});
+	});
+    });
 }
 
-function logoutHandler(){
-    var op = logout();
-    if(op == false){
-	alert("Error: login credentials not defined");
-    } else {
-	loggedin = false;
-	chrome.browserAction.setIcon({path:"icon_grey.png"});
+function logout(){
+    var storage = chrome.storage.local;
+    storage.get(null,function(data){
+	if(!data.user || !data.pass){
+	    console.log("Logout : crendentials not defined");
+	    alert("Error: login credentials not defined");
+	    return;
+	}
+	$.post(theUrl, { mode:"193", username:data.user }, function(response){
+	    console.log("Logout response :" + response);
+	    loggedin = false;
+	    chrome.browserAction.setIcon({path:"icon_grey.png"});
+	});
+    });
+}
+
+function checkConnection(callback){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, true);
+
+    xmlHttp.onreadystatechange=function(){
+	if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+	    clearTimeout(xmlHttpTimeout);
+	    console.log("Connection established with cyberoam");
+	    chrome.browserAction.setBadgeText({text:""});
+	    callback();
+   	}
+    }
+
+    xmlHttp.send("");
+    var xmlHttpTimeout=setTimeout(ajaxTimeout,2000);	// 2 seconds Timeout
+    function ajaxTimeout(){
+	xmlHttp.abort();
+   	alert("Hi !!, this is cyberoam autologin extension\nand I'm really sorry to say that I can't find cyberoam :(");
+   	// if cyberoam is unreachable then there is no point in checking for it again & again.
+   	//clearInterval(timer);
+	console.log("Connection failed");
+	chrome.browserAction.setBadgeText({text:"!"});
     }
 }
--- options.js
+++ options.js
@@ -1,5 +1,6 @@
+$('#check').hide();
 var storage = chrome.storage.local;
-var submit = document.querySelector('button.submit');
+var submit = document.getElementById('button');
 var user = document.getElementById('user');
 var pass = document.getElementById('pass');
 
@@ -10,12 +11,14 @@ submit.addEventListener('click',saveData);
 function saveData(){
     var username = user.value;
     var password = pass.value;
-    alert("user = " + user.value + "pass ="+pass.value);
+    console.log("user = " + user.value + "pass ="+pass.value);
     if(!username || !password){
 	return;
     }
     
     storage.set({'user':username, 'pass':password});
+    $('#check').fadeIn("slow");
+    $('#check').delay(400).fadeOut("slow");
 }
 
 function loadData(){
