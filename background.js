59aa17c626d29417c80ecb2a9fe58a30f7e71bb9
jaydp17 <j.solanki17@yahoo.com>
2013-01-03 04:45:57 +0530

    implement relogin

--- background.js
+++ background.js
@@ -1,8 +1,13 @@
 // this js file will be loaded when the browser starts
 
 var loggedin = false;
+var allowAuto = true;
+var liveTime = 60;	// this is in minutes
+var liveTimeFrmServer = true;
+
 //var theUrl = "http://10.100.56.55:8090/httpclient.html";
-var theUrl = "http://localhost/cyb.php";
+//var theUrl = "http://localhost/cyb.php";
+var theUrl = "http://localhost/cyb_ankit.html";
 var storage = chrome.storage.local;
 
 checkConnection(login);
@@ -11,7 +16,9 @@ checkConnection(login);
 chrome.browserAction.onClicked.addListener(function(tab){
     if(loggedin){
 	checkConnection(logout);
+	allowAuto = false;
     } else {
+	allowAuto = true;
 	checkConnection(login);
     }
 } );
@@ -28,6 +35,9 @@ function login(){
 	    console.log("Login response :" + response);
 	    loggedin = true;
 	    chrome.browserAction.setIcon({path:"icon.png"});
+	    setTimeout(function(){
+		checkConnection(login);
+	    },liveTime*60*1000);	// Set the time here 
 	});
     });
 }
@@ -49,6 +59,12 @@ function logout(){
 }
 
 function checkConnection(callback){
+    
+    // if auto login is not allowed return back
+    if(!allowAuto){
+	return;
+    }
+    
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", theUrl, true);
 
@@ -57,6 +73,17 @@ function checkConnection(callback){
 	    clearTimeout(xmlHttpTimeout);
 	    console.log("Connection established with cyberoam");
 	    chrome.browserAction.setBadgeText({text:""});
+	    
+	    if(liveTimeFrmServer){
+		// gets the liveLogin time from cyberoam
+		var source = xmlHttp.responseText;
+		var indxA = source.indexOf("liveReqTimeInJS");
+		var indxEq = source.indexOf("=",indxA);
+		var indxB = source.indexOf(";",indxEq);
+		var sub = source.substring(indxEq+1,indxB);
+		liveTime = parseInt(sub,10);
+	    }
+	    // & finally executes the callback
 	    callback();
    	}
     }
@@ -70,5 +97,6 @@ function checkConnection(callback){
    	//clearInterval(timer);
 	console.log("Connection failed");
 	chrome.browserAction.setBadgeText({text:"!"});
+	allowAuto = false;
     }
 }
--- options.js
+++ options.js
@@ -3,10 +3,23 @@ var storage = chrome.storage.local;
 var submit = document.getElementById('button');
 var user = document.getElementById('user');
 var pass = document.getElementById('pass');
+var interval = document.getElementById('interval');
 
 loadData();
 
 submit.addEventListener('click',saveData);
+
+$(':radio').change(function() {
+    if($(this).val() === "opt1"){
+       interval.disabled = false;
+       interval.focus();
+       chrome.extension.getBackgroundPage().liveTimeFrmServer = false;
+    } else {
+       interval.disabled = true;
+       chrome.extension.getBackgroundPage().liveTimeFrmServer = true;
+    }
+});
+
 
 function saveData(){
     var username = user.value;
@@ -15,13 +28,17 @@ function saveData(){
     if(!username || !password){
 	return;
     }
-    
+    if(!chrome.extension.getBackgroundPage().liveTimeFrmServer){
+	chrome.extension.getBackgroundPage().liveTime = parseInt(interval.value);
+    }
     storage.set({'user':username, 'pass':password});
     $('#check').fadeIn("slow");
     $('#check').delay(400).fadeOut("slow");
 }
 
 function loadData(){
+    var lTime = chrome.extension.getBackgroundPage().liveTime;
+    interval.value = lTime.toString();
     storage.get(null,function(data){
 	if(!data.user || !data.pass){
 	    return;
@@ -30,3 +47,8 @@ function loadData(){
 	pass.value = data.pass;
     });
 }
+
+$('#interval').jStepper({minValue:30, maxValue:720});
+$('#interval').bind("cut copy paste",function(e) {
+    e.preventDefault();
+});
